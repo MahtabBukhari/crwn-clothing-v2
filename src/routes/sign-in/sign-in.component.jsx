b57d4b5da75279eff1d85143bea15f913d4d48e8
MahtabBukhari <git config --global user.email you@example.com>
2022-12-11 11:45:55 +0500

    sign with google by the help of firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { signInWithGooglePopup } from '../../utiles/firebase/firebase.component.utiles'
 
 const SignIn = () => {
+  const loginWithGoogle=async()=>{
+        
+    const response = await signInWithGooglePopup()
+    console.log(response)
+  }
   return (
+    <div>
     <div>SignIn page</div>
+    <button type="button" onClick={loginWithGoogle}>sign in</button>
+
+    </div>
   )
 }
 
