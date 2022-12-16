c7f63c810d79e60286438a5bde4628757bddad6b
MahtabBukhari <git config --global user.email you@example.com>
2022-12-16 18:09:10 +0500

    (important) using css in styled-component and style the form

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import './form-input.styles.scss'
+import {Group,FormInputField,FormInputLabel} from './form-input.styles';
 
 const FormInput = ({label, ...otherProps}) => {
   return (
-    <div className='group'>
-    <input className='form-input'
+    <Group>
+    <FormInputField
     {...otherProps}
     />
-    {label && <label className={`${otherProps.value.length ? 'shrink':''} form-input-label`}>{label}</label>}
-    </div>
+    {label && <FormInputLabel shrink={otherProps.value.length}>{label}</FormInputLabel>}
+    </Group>
   )
 }
 
