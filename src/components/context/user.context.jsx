658026f957f1a2f11433c256885bb5bc9f3c14d5
MahtabBukhari <git config --global user.email you@example.com>
2022-12-18 23:30:45 +0500

    useReducer is used to manage the user state instead of useState hooks

--- src/components/context/user.context.jsx
+++ src/components/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer} from "react";
 import { createUserDocumentfromAuth, onAuthStateChangedListener } from "../../utiles/firebase/firebase.utiles";
 
 const userContext = createContext({
@@ -6,9 +6,41 @@ const userContext = createContext({
   setCurrentUser: () => null,
 });
 
+const setCurrentUserConstantType={
+  SET_CURRENT_USER:"SET_CURRENT_USER"
+
+}
+
+const userReducer =(state,action)=>{
+const {type,payload}=action
+  switch(type){
+    case setCurrentUserConstantType.SET_CURRENT_USER:
+
+    return{
+      ...state,
+      currentUser:payload
+    }
+    default:
+      throw new Error('type is not defined in userReducer')
+  }
+
+}
+
+const INITIAL_STATE={
+  currentUser:null
+}
+
+
 export const UserContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+ const [state,dispatch]=useReducer(userReducer,INITIAL_STATE)
+
+ const {currentUser}= state
+
+ const setCurrentUser=(user)=>{
+  dispatch({type:setCurrentUserConstantType.SET_CURRENT_USER,payload:user})
+ }
+
+  const value = { currentUser};
 
   useEffect(()=>{
    const unSubscribe =onAuthStateChangedListener((user)=>{
