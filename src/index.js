eff8b267970815a7d4067e9fab78b2996c1b8cc9
MahtabBukhari <git config --global user.email you@example.com>
2022-12-24 19:47:45 +0500

    persist local state to local storrage by using the redux-persist library

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store ,persistor} from "./store/store";
+
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <React.StrictMode>
   <Provider store={store}>
+  <PersistGate persistor={persistor}>
+ 
     <BrowserRouter>
      
       
@@ -19,6 +23,7 @@ ReactDOM.render(
       
      
     </BrowserRouter>
+    </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,22 @@
 import {compose, createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
+import {persistReducer,persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig={
+    key:'root',
+    storage,
+    blackList:['user']
+}
+
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 const middleWare = [logger]
 
 const composeEnhancers = compose(applyMiddleware(...middleWare))
 
-export const store = createStore(rootReducer,undefined,composeEnhancers)
+export const store = createStore(persistedReducer,undefined,composeEnhancers)
+export const persistor = persistStore(store)
 
 
